4bd7ef04e76d5e1beee66246eab4f44d9852dbbe
Casey Faist <caseylfaist@gmail.com>
2021-02-04 17:23:08 -0500

    Update tests to check for auth

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,10 +1,15 @@
 from app import app
+from os import environ
+
+SERVER_KEY=environ.get("SERVER_KEY")
 
 def test_echo():
     """Test the echo function"""
 
     test_string = "A string to test that our endpoint echos"
-    response = app.test_client().post('/', data=test_string)
+    response = app.test_client().post('/', 
+                                        headers=dict(server_key=SERVER_KEY), 
+                                        data=test_string)
 
     assert response.status_code == 200
     assert response.data == test_string.encode('utf-8')
--- tests/test_is_live.py
+++ tests/test_is_live.py
@@ -1,8 +1,28 @@
 from app import app
+import pytest
+from os import getenv
 
-def test_ping():
+
+def test_no_key_ping():
     """Check that the root endpoint is live"""
 
     response = app.test_client().get('/')
+    assert "server_key" not in response.headers
+    assert response.status_code == 403
+    assert response.data == "Please add server_key to access".encode("UTF_8")
+
+def test_wrong_key_ping():
+    """Check that the root endpoint is live"""
+
+    response = app.test_client().get('/', headers=dict(server_key="bla"))
+    assert response.status_code == 403
+    assert response.data == "Incorrect server key".encode("UTF_8")
+
+def test_correct_key_ping():
+    """Check that the root endpoint is live"""
+
+    SERVER_KEY = getenv('SERVER_KEY')
+    response = app.test_client().get('/', headers=dict(server_key=SERVER_KEY))
     assert response.status_code == 200
+    assert response.data != "Incorrect server key".encode("UTF_8")
     assert response.data == "Hello! I will echo whatever you send me!".encode("UTF_8")
